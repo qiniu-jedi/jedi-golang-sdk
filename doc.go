bca6387bdd756caf6f0f6a7a5f76af530b1c16b5
tianyang <tianzong48@gmail.com>
2016-11-01 14:28:47 +0800

    doc.go

---
+++ doc.go
@@ -0,0 +1,21 @@
+/*
+使用方法
+	var c jedi.ConfQiniu
+	c.SetMac("your_ak", "you_sk")
+
+	//获取uptoken
+	res,err := jedi.GetUpToken(c, "hubname", "", 86400)
+	if err !=nil{
+		//Code
+	}
+
+
+	//删除视频 
+	res2,err := jedi.DeleteVideo(c,"hubname", "key")
+	if err !=nil{
+		//Code
+	}
+	...
+
+
+*/
